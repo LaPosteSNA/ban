578c21eae9c54613dff3d98ad7dcb7ba2db8a192
Apside-bdx-BBO <bonheur.0692@apside-groupe.com>
2015-12-08 10:48:51 +0100

    a exportation trial to addok

--- ban/commands/addok.py
+++ ban/commands/addok.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
-from ban.commands import command, report
+from peewee import RawQuery, fn
 
+from ban.commands import command, report
 from ban.core import models
 from ban.core.encoder import dumps
+
 
 @command
 def export_all(path, **kwargs):
@@ -14,7 +16,21 @@ def export_all(path, **kwargs):
     resources = [models.HouseNumber]
     with Path(path).open(mode='w', encoding='utf-8') as f:
         for resource in resources:
-            for data in models.Street.select().join(models.Municipality).where(models.Street.municipality_id == models.Municipality.id).as_resource_list(): # .where(models.HouseNumber.street == models.Street.identifiers ).as_resource_list():
-                f.write(dumps(data) + '\n')
-                # Memory consumption when exporting all France housenumbers?
-                report(resource.__name__, data)
+            resco = {}
+            for data in models.Street.select().as_resource():
+                resco = data
+                resco['type'] = models.Street.__name__
+                resco['housenumbers'] = []
+                print(data.get('id'))
+                rec = {}
+                for dat in models.HouseNumber.select().where(models.HouseNumber.street == data.get('id')).as_resource():
+                    for da in dat:
+                        rec = {dat['number'], da}
+                    resco['housenumbers'].append(rec)
+                write_to_file(resco, f, resource)
+
+
+def write_to_file(data, f, resource):
+    f.write(dumps(data) + '\n')
+    # Memory consumption when exporting all France housenumbers?
+    report(resource.__name__, data)
